00b297a3ec144a9fc87a704c881ac2e7cf70265f
Reik Loeber <reik.loeber@gmail.com>
2025-09-25 19:22:15 +0200

    Fix: Variable name

--- conduit-frontend/entrypoint.sh
+++ conduit-frontend/entrypoint.sh
@@ -6,7 +6,7 @@ ENV_FILE=/usr/share/nginx/html/assets/env-config.js
 cat <<EOF > $ENV_FILE
 (function (window) {
   window.__env = window.__env || {};
-  window.__env.apiUrl = "http://${SERVERIP}:${BACKENDPORT}";
+  window.__env.apiUrl = "http://${SERVERIP}:${BACKEND_PORT}";
 })(this);
 EOF
 
